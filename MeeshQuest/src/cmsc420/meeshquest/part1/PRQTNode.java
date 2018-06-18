a67ead1f2c603c4fbe3778664856e6302037e450
Anna Blendermann <annablender91@gmail.com>
2018-06-18 15:09:45 -0400

    wrote pr quadtree node and wrapper classes

--- MeeshQuest/src/cmsc420/meeshquest/part1/PRQTNode.java
+++ MeeshQuest/src/cmsc420/meeshquest/part1/PRQTNode.java
@@ -1,11 +1,44 @@
 package cmsc420.meeshquest.part1;
+import java.awt.geom.Point2D;
 
+/**
+ * Abstract node class for the PR QuadTree data structure
+ * think of it as: default functions
+ */
 public abstract class PRQTNode {
 
-    /*
-    TO DO: create abstract class for a pr quadtree node
-    write getters, setters, insert, delete, and clear functions
-     */
 
+    /**
+     * Returns true or false based on whether a city is in the quadtree
+     * @return
+     */
+    public boolean contains(City city) {
+
+        return false;
+    }
+
+
+    /**
+     * Inserts a PRQTNode (white, black, or gray) into the quadtree
+     * @param city
+     * @param center
+     * @param dim
+     * @return
+     */
+    public PRQTNode insert(City city, Point2D.Float center, int dim) {
+
+        return new WhiteNode();
+    }
+
+
+    /**
+     * Deletes a PRQTNode (white, gray, or black) from the quadtree
+     * @param city
+     * @return
+     */
+    public static WhiteNode delete(City city, Point2D.Float origin, int dim) {
+
+        return new WhiteNode();
+    }
 
 }
--- MeeshQuest/src/cmsc420/meeshquest/part1/PRQuadTree.java
+++ MeeshQuest/src/cmsc420/meeshquest/part1/PRQuadTree.java
@@ -1,4 +1,50 @@
 package cmsc420.meeshquest.part1;
+import java.awt.geom.Point2D;
 
+
+/**
+ * Data structure class for the PR QuadTree
+ * Serves as a wrapper class for the spatial data structure
+ */
 public class PRQuadTree {
+
+
+    private PRQTNode root = null;
+
+
+    /**
+     * Constructs the PR QuadTree spatial data structure
+     */
+    public PRQuadTree() {
+
+        root = new WhiteNode();
+    }
+
+
+    /**
+     * Returns the root of the PR QuadTree
+     * @return
+     */
+    public PRQTNode getRoot() {
+
+        return this.root;
+    }
+
+
+    /**
+     * Inserts a city point into the quadtree
+     * @param city = City object
+     * @return
+     */
+    public PRQTNode insert(City city, Point2D.Float center, int dim, int level) {
+
+        /*
+        What happens here?
+        - white node: inserts the city into the white node -> black node
+        - black node: creates a gray node and inserts the city into the correct child node
+        - gray node: inserts the city into the correct child node
+         */
+        return this.root.insert(city, center, dim);
+    }
+
 }
